96281f120f3788c0a60ed456b541bafef0396386
Manoj <manojgopalraj1996@gmail.com>
2026-02-11 09:04:38 +0530

    unchanged

--- while.sh
+++ while.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-#echo "enter 4 arguments"
+#echo "enter only 4 arguments"
 
 
 var1=$1
 var2=$2
 var3=$3
 var4=$4
-#read $1 $2 $3 $4
-#exit 1
+
 if [ $# -ne 4 ]; then
      echo "passs only 4 args"
     exit 1
